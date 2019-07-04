aaa9d9514e8cc9ed15f6524071f77793d9469a59
Moustafa <bendib.moustafa@gmail.com>
2019-07-04 11:32:42 +0100

    Add delete post feature

--- blog/views.py
+++ blog/views.py
@@ -59,6 +59,14 @@ def post_publish(request, pk):
     post=get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
+@login_required
+def delete_post(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('/', pk=post.pk)
+    
+    
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -85,3 +93,4 @@ def comment_approve(request, pk):
     comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
